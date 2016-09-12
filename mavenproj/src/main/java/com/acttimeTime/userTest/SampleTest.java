9ef95e2389b1b95637b122d327b0d0614e2e2efb
SIBU <SIBU@192.168.2.8>
2016-09-12 21:04:19 +0530

    A new testng update

--- mavenproj/src/main/java/com/acttimeTime/userTest/SampleTest.java
+++ mavenproj/src/main/java/com/acttimeTime/userTest/SampleTest.java
@@ -1,13 +1,18 @@
 package com.acttimeTime.userTest;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+import org.testng.xml.XmlTest;
 public class SampleTest {
 	@Test
-	public void login(){
-		System.out.println("hello");
-		System.out.println("hi");
-		System.out.println("sibu");
-		System.out.println("somu");
+	public void login(XmlTest t){
+		WebDriver d=new FirefoxDriver();
+		d.get(t.getParameter("url"));
+		d.findElement(By.xpath(".//*[@id='username']")).sendKeys(t.getParameter("username"));
+		d.findElement(By.xpath(".//*[@id='loginFormContainer']/tbody/tr[1]/td/table/tbody/tr[2]/td/input")).sendKeys(t.getParameter("password"));
+		d.findElement(By.xpath(".//*[@id='loginButton']/div")).click();
 	}
 
 }
